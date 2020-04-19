c400318812ea584529e96c7d7f20a0e214adae3a
amir-alaj <aalaj@ucsc.edu>
2020-04-19 16:02:36 -0700

    added comments

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+/*
+Amir Alaj : aalaj
+(10) Track highscore that persists across screens and is displayed in UI
+(10) Allow player to control rocket after fired
+(15) Display time (in seconds) on the screen
+(15) Create new animated sprites for spaceships
+(25) Create new spaceship type that is smaller, moves faster, worth more points, and worth more time
+(25) Implement new timing/scoring mechanism that adds time on succesful hits.
+*/
+
 let config = {
     type: Phaser.CANVAS, 
     width: 640,
